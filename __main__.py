80ceef9fc6eb3bf97a43377712ae08d29bc2de6b
Isaac Rayment <isaacrayment123@gmail.com>
2021-03-21 18:12:35 +0000

    added fodler for settings page files

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from tex_py_gui.browser import browser
 from tex_py_gui.capture import capture
+from tex_py_gui.settings import settings
 from kivy.lang import Builder
 kivy.require("2.0.0")
 
@@ -26,10 +27,6 @@ class HomePage(Screen):
         app.sm.current = "Settings"
 
 
-class SettingsPage(Screen):
-    pass
-
-
 class TextileGui(App):
 
     def build(self):
@@ -42,7 +39,7 @@ class TextileGui(App):
         self.sm.add_widget(capture.NewCapPage(name="New Capture"))
         self.sm.add_widget(browser.ProjectBrowserPage(name="Project Browser"))
         self.sm.add_widget(browser.ProjectViewPage(name="Project View"))
-        self.sm.add_widget(SettingsPage(name="Settings"))
+        self.sm.add_widget(settings.SettingsPage(name="Settings"))
 
         return self.sm
 
